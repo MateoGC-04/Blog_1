b0d8882c0f6a1453bd9d9f5e912c11c6be9d5d05
MateoGC-04 <112424343+MateoGC-04@users.noreply.github.com>
2023-02-22 10:27:59 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+index.js
+function cambiar1()
+{
+	document.getElementById('body1').style.background = "linear-gradient(#FF6347, #000000)"; 
+}
+
+function cambiar2()
+{
+	document.getElementById('body1').style.background = "linear-gradient(#FFD700, #000000)";
+}
+
+function cambiar3()
+{
+	document.getElementById('body1').style.background = "linear-gradient(#ADFF2F, #000000)";
+}
+
+function cambiarvuelta()
+{
+	document.getElementById('body1').style.background = "linear-gradient(#000000, #000000)"; 
+}
